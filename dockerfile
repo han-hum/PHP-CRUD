74d8c6e5d45a4540ad890df836b29d6c1a5a69b4
hanna <hanna.hummal@khk.ee>
2022-11-22 18:30:02 +0200

    added htaccess and a2enmod

--- dockerfile
+++ dockerfile
@@ -1,4 +1,5 @@
 FROM php:7.4-apache
+RUN a2enmod rewrite
 RUN docker-php-ext-install mysqli
 
 COPY src/ /var/www/html
--- src/index.php
+++ src/index.php
@@ -9,20 +9,20 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method == 'GET') { //connection to db
 
-    $con = mysqli_connect('db', 'test', 'test', 'fakeStore') or 
-            die("Connection failed: " . mysqli_error());
-        
+    $con = mysqli_connect('db', 'test', 'test', 'fakeStore') or
+        die("Connection failed: " . mysqli_error($con));
+
 
     $sql = "SELECT * FROM products"; //get
-    $result = mysqli_query($con, $sql) or die ("Error in selecting"
-    . mysqli_error($con));
-    
+    $result = mysqli_query($con, $sql) or die("Error in selecting"
+        . mysqli_error($con));
+
     $productArr = array();
     while ($row = mysqli_fetch_assoc($result)) {
         $productArr[] = $row;
     }
 
-    echo json_encode($productArr); 
+    echo json_encode($productArr);
 
     mysqli_close($con); //close connection
 
@@ -36,17 +36,17 @@ if ($method == 'GET') { //connection to db
     $price = $jsonData->price;
     $category = $jsonData->category;
 
-    $con = mysqli_connect('db', 'test', 'test', 'fakeStore') or 
-    die("Connection failed: " . mysqli_error());
+    $con = mysqli_connect('db', 'test', 'test', 'fakeStore') or
+        die("Connection failed: " . mysqli_error());
 
     $sql = "INSERT INTO products (title, price, category) VALUES ('$title', '$price', '$category')";
-    
+
     if (mysqli_query($con, $sql)) {
         $last_id = mysqli_insert_id($con);
 
         $mes = array("id" => $last_id, "title" => $title, "price" => $price, "category" => $category);
-    
-    echo json_encode($mes, JSON_UNESCAPED_SLASHES);
+
+        echo json_encode($mes, JSON_UNESCAPED_SLASHES);
     } else {
         echo "Can't add a new product";
     }
@@ -64,8 +64,8 @@ if ($method == 'GET') { //connection to db
     $price = $jsonData->price;
     $category = $jsonData->category;
 
-    $con = mysqli_connect('db', 'test', 'test', 'fakeStore') or 
-    die("Connection failed: " . mysqli_error());
+    $con = mysqli_connect('db', 'test', 'test', 'fakeStore') or
+        die("Connection failed: " . mysqli_error());
 
     $sql = "UPDATE products SET title='$title', price='$price', category='$category' WHERE id = '$id'";
 
@@ -73,19 +73,19 @@ if ($method == 'GET') { //connection to db
         # $last_id = mysqli_insert_id($con);
 
         $mes = array("id" => $id, "title" => $title, "price" => $price, "category" => $category);
-    
-    echo json_encode($mes, JSON_UNESCAPED_SLASHES);
+
+        echo json_encode($mes, JSON_UNESCAPED_SLASHES);
     } else {
         echo "Can't update a product";
     }
     mysqli_close($con); //close connection 
 
 } elseif ($method == 'DELETE') {
-    
+
     $id = (int)$_GET['id'];
 
-    $con = mysqli_connect('db', 'test', 'test', 'fakeStore') or 
-    die("Connection failed: " . mysqli_error());
+    $con = mysqli_connect('db', 'test', 'test', 'fakeStore') or
+        die("Connection failed: " . mysqli_error());
 
     $sql = "DELETE FROM products WHERE id='$id'";
 
@@ -97,12 +97,12 @@ if ($method == 'GET') { //connection to db
 
     mysqli_close($con); //close connection 
 
-    }
+} elseif ($method == 'OPTIONS') {
 
-    else {
-        echo json_encode(
-            array('message'=>'method unknown')
-        );
-    }
-?>
 
+} else {
+    echo json_encode(
+        array('message' => 'method unknown')
+    );
+}
+   #{"title": "123456", "price": "123456", "category": "1234567890"}
